96c773b4e866c9e89376eee38e2af14c04aa264e
Vinicius Guedes <viniciusgued@gmail.com>
2023-06-18 21:24:54 -0300

    feat(services): UserService.UpdateProfile

---
+++ services/user_service.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"context"
+
+	"verifymy-golang-test/common"
+	"verifymy-golang-test/models"
+	"verifymy-golang-test/repositories"
+	"verifymy-golang-test/utils"
+)
+
+type UserService interface {
+	UpdateProfile(ctx context.Context, attributes models.User) error
+}
+
+type userService struct {
+	userRepository repositories.UserRepository
+}
+
+func NewUserService(
+	userRepository repositories.UserRepository,
+) UserService {
+	return &userService{
+		userRepository: userRepository,
+	}
+}
+
+func (s *userService) UpdateProfile(ctx context.Context, attributes models.User) error {
+	user := ctx.Value(common.AuthUser).(*models.User)
+	if attributes.Password != "" {
+		hashedPassword, err := utils.PasswordHash(string(attributes.Password))
+		if err != nil {
+			return err
+		}
+
+		attributes.Password = models.SecretValue(hashedPassword)
+	}
+
+	return s.userRepository.UpdateAttributesByUserId(ctx, user.ID.String(), attributes)
+}
---
+++ services/user_service_test.go
@@ -0,0 +1,91 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/suite"
+
+	"verifymy-golang-test/common"
+	mock_repositories "verifymy-golang-test/mocks/repositories"
+	"verifymy-golang-test/models"
+)
+
+type userServiceTestSuite struct {
+	suite.Suite
+	ctrl               *gomock.Controller
+	userRepositoryMock *mock_repositories.MockUserRepository
+	service            UserService
+}
+
+func TestUserServiceTestSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(userServiceTestSuite))
+}
+
+func (s *userServiceTestSuite) SetupTest() {
+	s.ctrl = gomock.NewController(s.T())
+	s.userRepositoryMock = mock_repositories.NewMockUserRepository(s.ctrl)
+	s.service = NewUserService(s.userRepositoryMock)
+}
+
+func (s *userServiceTestSuite) TestUpdateProfile() {
+	userId := uuid.New()
+	user := &models.User{
+		ID: userId,
+	}
+
+	tests := []struct {
+		description                   string
+		attributes                    models.User
+		updatingPassword              bool
+		updateAttributesByUserIdError error
+	}{
+		{
+			description: "Success",
+			attributes: models.User{
+				Name: "John Doe",
+			},
+		},
+		{
+			description: "Success changing password",
+			attributes: models.User{
+				Password: "my-password",
+			},
+			updatingPassword: true,
+		},
+		{
+			description: "Error updating attributes by user id",
+			attributes: models.User{
+				Name: "John Doe",
+			},
+			updateAttributesByUserIdError: errors.New("error"),
+		},
+	}
+
+	for _, test := range tests {
+		s.Run(test.description, func() {
+			ctx := context.Background()
+			ctx = context.WithValue(ctx, common.AuthUser, user)
+
+			var attributesCopy interface{} = test.attributes
+			if test.updatingPassword {
+				attributesCopy = gomock.Any()
+			}
+
+			s.userRepositoryMock.EXPECT().UpdateAttributesByUserId(
+				ctx, userId.String(), attributesCopy,
+			).Return(test.updateAttributesByUserIdError)
+
+			err := s.service.UpdateProfile(ctx, test.attributes)
+			if test.updateAttributesByUserIdError != nil {
+				s.Error(err)
+			} else {
+				s.NoError(err)
+			}
+		})
+	}
+}
